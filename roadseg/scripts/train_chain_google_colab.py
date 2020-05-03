04d51a1dbd75844fef96f27e3b4ba39f48676d52
mmarouen <azzouz.marouen@gmail.com>
2020-05-03 13:17:59 +0200

    train and save model

--- roadseg/scripts/train_chain_google_colab.py
+++ roadseg/scripts/train_chain_google_colab.py
@@ -1,5 +1,7 @@
 import os
 import argparse
+from keras.optimizers import Adam
+import matplotlib as plt
 from roadseg.utils.config_loader import ConfigReader
 from roadseg.utils.data_utils import DataUtils
 from roadseg.models.fcn_models import FcnModel
@@ -14,18 +16,41 @@ def train_model(config: ConfigReader):
 
     # load data
     data_handler = DataUtils(config.data_path, config.weights_path)
-    print("nclass %i" %config.n_class)
     X_train,y_train = data_handler.get("data/training", config.n_class, config.input_height, config.input_width)
-    
     if config.augment_data:
         X_train,y_train = data_handler.augment_training_data(X_train,y_train, config.n_class, config.input_height, config.input_width)
     print("training data shape: {0}".format(X_train.shape))
+
+    X_val, y_val = data_handler.get("data/validation", config.n_class, config.input_height, config.input_width)
+    print("validation data shape: {0}".format(X_val.shape))
 
     # build model
     if config.model_name =="fcn8":
         model_version = 8
     fcn_model = FcnModel(config.n_class, config.input_height, config.input_width, config.weights_path)
-    print(fcn_model.model.summary())
+    fcn_model.model.compile(loss='binary_crossentropy',optimizer=optimizers.Adam(lr=1e-4),metrics=['acc'])
+    history = fcn_model.model.fit(x=X_train, y=y_train, epochs=20, batch_size=10, validation_data=(X_val, y_val))
+
+    acc = history.history['acc']
+    val_acc = history.history['val_acc']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    epochs = range(len(acc))
+
+    plt.plot(epochs, acc, 'bo', label='Training acc')
+    plt.plot(epochs, val_acc, 'b', label='Validation acc')
+    plt.title('Training and validation accuracy')
+    plt.legend()
+    plt.figure()
+
+    plt.plot(epochs, loss, 'bo', label='Training loss')
+    plt.plot(epochs, val_loss, 'b', label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.legend()
+    plt.show()
+    fcn_model.model.save("fcn32.h5")
+
 
 def main():
     args = parse_arguments()
